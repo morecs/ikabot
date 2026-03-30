b4596bf2bd99151a65ae21cd018493c0c56b05bb
morecs <136636021+morecs@users.noreply.github.com>
2026-03-30 03:02:33 +0300

    Upgrade units (#368)
    Fix issue with duplicate upgrade.
    Add transport ship in the upgrade list.
    Added option to select the city where to do the upgrade.

--- ikabot/command_line.py
+++ ikabot/command_line.py
@@ -53,6 +53,7 @@ from ikabot.helpers.process import updateProcessList
 from ikabot.web.session import *
 from ikabot.function.UpgradeUnits import UpgradeUnits
 from ikabot.function.modifyProduction import modifyProduction
+from ikabot.function.UpgradeUnits import UpgradeUnits
 from ikabot.function.developer import developer
 
 
